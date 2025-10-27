2afaa319e978ee0c4a744c362a09281622e2085b
Reina Valerie G. Sarmiento <ronaldvincent.sarmiento@gmail.com>
2025-10-27 18:25:12 +0800

    Added Restart Menu and added Assertions

--- scripts/UI/Buttons/Templates/scene_changer_button.gd
+++ scripts/UI/Buttons/Templates/scene_changer_button.gd
@@ -3,4 +3,5 @@ extends Button
 @export var TargetScene : PackedScene
 
 func _on_button_up() -> void:
+	assert(TargetScene != null, "Error: You have not set a target scene for this button!")
 	get_tree().change_scene_to_packed(TargetScene)
